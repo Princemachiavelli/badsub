617f39aa1164c58063fd4101c610b2223e906670
Josh Hoffer <jhoffer@anduril.com>
2024-05-10 11:07:57 -0700

    current

--- flake.nix
+++ flake.nix
@@ -1,14 +1,20 @@
 {
   description = "A very basic flake";
 
-  outputs = { self, nixpkgs }: {
+  inputs = {
+    sub1.url = "git+ssh://git@github.com/Princemachiavelli/badsub?submodules=1";
+  };
+
+  outputs = { self, nixpkgs, sub1 }: {
 
     packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.stdenvNoCC.mkDerivation {
       name = "example";
       src = ./.;
       dontBuild = true;
       installPhase = ''
-        cp ./submodule_a/README.md $out 
+        mkdir -p $out
+        cp ./submodule_a/README.md $out/SUBMODULE_A_README.md 
+	cp ${sub1}/submodule_a/README.md $out/sub1_readme.md
       '';
     };
 
